fa9cb02cf8d43f0115123b4242e9df032c641574
DanielLeczkowski <daniel.leczkowski@wp.pl>
2017-10-20 01:35:08 +0200

    Add table with data users

--- js/main.js
+++ js/main.js
@@ -5,5 +5,44 @@ $(document).ready(function(){
     var currentDivNumber = 0;
     var currentDiv = 'a';
     var divTable = ['a', 'b', 'c', 'd', 'e', 'f'];
+    let users = [{
+        name: 'Jan Kowalski',
+        age: 18,
+        phone: '+48 787 693 128',
+        class: 'a'
+    },
+    {
+        name: 'Adam Nowak',
+        age: 25,
+        phone: '+48 569 235 564',
+        class: 'b'
+    },
+    {
+        name: 'Anna Pawłowska',
+        age: 30,
+        phone: '+48 679 452 545',
+        class: 'c'
+        
+    },
+    {
+        name: 'Maria Sosnowska',
+        age: 24,
+        phone: '+48 789 231 231',
+        class: 'd'
+    },
+    {
+        name: 'Paweł Wróblewski',
+        age: 29,
+        phone: '+48 589 741 123',
+        class: 'e'
+        
+    },
+    {
+        name: 'Katarzyna Słomińska',
+        age: 32,
+        phone: '+48 600 987 325',
+        class: 'f'
+    }
+    ];
 });
 
